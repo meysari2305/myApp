0c38a6288d28e74b9b5ac993ceaae980a3714e8d
Iis Meysari <iismeysari230599@gmail.com>
2022-09-25 16:21:27 +0700

    inisial commit

---
+++ src/materi/komponen/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classComponent from './pembahasan/classComponent.jsx';
+
+export default class komponen extends React.Component {
+  render() {
+    return (
+      <div>
+        <classComponent />
+      </div>
+    );
+  }
+}
---
+++ src/materi/komponen/pembahasan/classComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class classComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>komponen ini dibuat dengan class component</h1>
+        <h2> Hello Meysari</h2>
+      </div>
+    );
+  }
+}
+
+export default classComponent;
